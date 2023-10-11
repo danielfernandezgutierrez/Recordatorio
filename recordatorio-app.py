c168f239e7953c7b29d3f259e2d43fa8af2d5c21
danielfernandezgutierrez <147178981+danielfernandezgutierrez@users.noreply.github.com>
2023-10-11 18:24:25 -0300

    recordatorio-app.py

--- recordatorio-app.py
+++ recordatorio-app.py
@@ -6,7 +6,7 @@ try:
        con = sql.connect('recordatorio_db.db')
        cur = con.cursor()
        cur.execute('''CREATE TABLE notes_table
-                        (date date, notes_title text, notes text)''')
+                        (date text, notes_title text, notes text)''')
 except:
        print("Conectado a la Base de Datos")
 # Insert a row of data
@@ -47,18 +47,19 @@ def view_notes():
        cur.execute(sql_statement)
        #Obtain all the contents of the query
        row = cur.fetchall()
+       print(row)
        #Check if none was retrieved
        if len(row)<=0:
                messagebox.showerror(message="No se encontro nigun recordatorio")
        else:
                #Print the notes
                for i in row:
-                       messagebox.showinfo(message="fecha recordatorio: "+i[0]+"\nTitulo: "+i[1]+"\nRecordar: "+i[2])
+                       messagebox.showinfo(message="fecha: "+i[0]+"\nTitulo: "+i[1]+"\nRecordar: "+i[2])
 #Delete the notes
 def delete_notes():
-            #Obtain input values
+        #Obtain input values
        f_aviso = fecha_entry.get()
-       texto_titulo = titulo_entry.get()
+       titulo = titulo_entry.get()
        #Ask if user wants to delete all notes
        choice = messagebox.askquestion(message="Esta seguro de borrar la nota?")
        #If yes is selected, delete all
@@ -66,12 +67,12 @@ def delete_notes():
                sql_statement = "DELETE FROM notes_table" 
        else:
        #Delete notes matching a particular date and title
-               if (len(f_aviso) <=0) & (len(notes_title)<=0): 
+               if (len(f_aviso) <=0) & (len(titulo)<=0): 
                        #Raise error for no inputs
                        messagebox.showerror(message = "Ingrese los detalles requeridos" )
                        return
                else:
-                      sql_statement = "DELETE FROM notes_table where date ='%s' and notes_title ='%s'" %(date, notes_title)
+                      sql_statement = "DELETE FROM notes_table where date ='%s' and notes_title ='%s'" %(f_aviso, titulo)
        #Execute the query
        cur.execute(sql_statement)
        messagebox.showinfo(message="Recordatorio(s) Borrado")
@@ -79,15 +80,15 @@ def delete_notes():
 #Update the notes
 def update_notes():
        #Obtain user input
-       fecha = fecha_entry.get()
-       titulo = texto_titulo_entry.get()
+       f_aviso = fecha_entry.get()
+       titulo = titulo_entry.get()
        texto = texto_entry.get("1.0", "end-1c")
        #Check if input is given by the user
-       if (len(fecha) <=0) & (len(titulo)<=0) & (len(texto)<=1):
+       if (len(f_aviso) <=0) & (len(titulo)<=0) & (len(texto)<=1):
                messagebox.showerror(message = "Ingrese los detalles requeridos" )
        #update the note
        else:
-               sql_statement = "UPDATE notes_table SET notes = '%s' where date ='%s' and notes_title ='%s'" %(notes, today, notes_title)
+               sql_statement = "UPDATE notes_table SET notes = '%s' where date ='%s' and notes_title ='%s'" %(texto, f_aviso, titulo)
               
        cur.execute(sql_statement)
        messagebox.showinfo(message="Recordatorio actualizado")
@@ -106,23 +107,23 @@ titulo_label = Label(window, text="Recordatorio - RSam").pack()
 
 #Leer inputs
 #Datos input
-fecha_label = Label(window, text="fecha recordatorio:").place(x=10,y=20)
+fecha_label = Label(window, text="fecha:").place(x=10,y=20)
 fecha_entry = Entry(window,  width=20)
 fecha_entry.place(x=50,y=20)
 #titulo recordatorio input
-titulo_label = Label(window, text="recordatorio titulo:").place(x=10,y=50)
+titulo_label = Label(window, text="titulo:").place(x=10,y=50)
 titulo_entry = Entry(window,  width=30)
 titulo_entry.place(x=80,y=50)
 #texto input
-texto_label = Label(window, text="Notes:").place(x=10,y=90)
+texto_label = Label(window, text="Nota:").place(x=10,y=90)
 texto_entry = Text(window, width=50,height=5)
 texto_entry.place(x=60,y=90)
  
 #Perform notes functions
-button1 = Button(window,text='Add Notes', bg = 'Turquoise',fg='Red',command=add_notes).place(x=10,y=190)
-button2 = Button(window,text='View Notes', bg = 'Turquoise',fg='Red',command=view_notes).place(x=110,y=190)
-button3 = Button(window,text='Delete Notes', bg = 'Turquoise',fg='Red',command=delete_notes).place(x=210,y=190)
-button4 = Button(window,text='Update Notes', bg = 'Turquoise',fg='Red',command=update_notes).place(x=320,y=190)
+button1 = Button(window,text='Agregar Nota', bg = 'Turquoise',fg='Red',command=add_notes).place(x=10,y=190)
+button2 = Button(window,text='Mostrar Nota', bg = 'Turquoise',fg='Red',command=view_notes).place(x=110,y=190)
+button3 = Button(window,text='Borarr Nota', bg = 'Turquoise',fg='Red',command=delete_notes).place(x=210,y=190)
+button4 = Button(window,text='Actualizar Nota', bg = 'Turquoise',fg='Red',command=update_notes).place(x=320,y=190)
  
 #close the app
 window.mainloop()
